f23f68ec27ad40dab38d0f7a9264a1223da61d06
Chris Heisel <chris@heisel.org>
2010-11-14 13:56:05 -0500

    Proof of concept for dynamic schemas

--- jamsession/models.py
+++ jamsession/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from mongoengine import (
+    Document,
+    StringField,
+    ListField,
+    GenericReferenceField,
+    DictField,
+    EmailField,
+)
 
-# Create your models here.
+class DynamicSchema(Document):
+    title = StringField(unique=True, required=True)
+    name = StringField(unique=True, required=True)
+    schema = DictField()
+
+    def get_data_access_object(self):
+        return type(
+            self.name,
+            (Document, ),
+            { "name": StringField(),
+              "title": StringField(),
+              "email": EmailField(),
+            })
