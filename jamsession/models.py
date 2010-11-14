fce1454e4cb152356deaadaf8280d8c54b7b822f
Chris Heisel <chris@heisel.org>
2010-11-14 17:31:57 -0500

    Actual dynamic schema working

--- jamsession/models.py
+++ jamsession/models.py
@@ -1,22 +1,40 @@
 from mongoengine import (
     Document,
     StringField,
-    ListField,
-    GenericReferenceField,
-    DictField,
+    URLField,
     EmailField,
+    IntField,
+    FloatField,
+    BooleanField,
+    DateTimeField,
+    DictField,
 )
 
-class DynamicSchema(Document):
+
+class DataSetDefinition(Document):
     title = StringField(unique=True, required=True)
     name = StringField(unique=True, required=True)
     schema = DictField()
 
-    def get_data_access_object(self):
+    _field_type_translations = {
+        'string': StringField,
+        'url': URLField,
+        'email': EmailField,
+        'int': IntField,
+        'float': FloatField,
+        'bool': BooleanField,
+        'datetime': DateTimeField }
+
+    def _get_data_object_fields(self):
+        data_object_fields = {}
+        for name, field_type in self.schema.items():
+            data_object_fields[name] = self._field_type_translations.get(field_type, StringField)()
+
+        return data_object_fields
+
+    def get_data_object(self):
+        data_object_fields = self._get_data_object_fields()
         return type(
             self.name,
             (Document, ),
-            { "name": StringField(),
-              "title": StringField(),
-              "email": EmailField(),
-            })
+            data_object_fields,)
--- jamsession/tests.py
+++ jamsession/tests.py
@@ -1,23 +1,59 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class DataSetTest(TestCase):
+    def setUp(self):
+        from mongoengine import connect
+        connect('jamsession-unit-tests')
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def tearDown(self):
+        from mongoengine.connection import _get_db
+        db = _get_db()
+        db.drop_collection('jamsession-unit-tests')
 
->>> 1 + 1 == 2
-True
-"""}
+    def _get_target_class(self):
+        from jamsession.models import DataSetDefinition
+        return DataSetDefinition
+
+    def _make_one(self, *args, **kwargs):
+        return self._get_target_class()(*args, **kwargs)
+
+    def test_data_set_basic_schema(self):
+        """
+        A DataSetDefinition's get_data_set method
+        should return an object that has fields
+        for every key in it's schema dictionary.
+        """
+        import datetime
+
+        schema = dict(
+            name = 'string',
+            url = 'url',
+            email = 'email',
+            integer = 'int',
+            floater = 'float',
+            boolean = 'bool',
+            datetime = 'datetime')
+
+        sample_values = dict(
+            name = 'The Doctor',
+            url = 'http://www.doctorwho.com',
+            email = 'the_dr@tardis.time.vortex.co.uk',
+            integer = 42,
+            floater = 42.0,
+            boolean = True,
+            datetime = datetime.datetime(year=1963, month=11, day=23))
+
+        sample_definition = self._make_one(
+                title = 'Sample Defintion',
+                name = 'SampleData',
+                schema = schema)
+
+        SampleData = sample_definition.get_data_object()
+        s = SampleData(**sample_values)
+        s.save()
+
+        s = SampleData.objects.get(id=s.id)
+        for key, value in sample_values.items():
+            self.assert_(hasattr(s, key))
+            self.assertEqual(value, getattr(s, key))
 
