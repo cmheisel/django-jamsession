eaa955c3af8aa766d3dd5572eec09529fc965d7c
Chris <chris@heisel.org>
2010-11-17 20:37:37 -0500

    Added setup.py and version number

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-jamsession",
+    version = __import__("jamsession").__version__,
+    author = "Chris Heisel",
+    author_email = "chris@heisel.org",
+    description = ("A Django reusable app providing the ability for admin "
+        "users to create their own forms."),
+    long_description = open("README.txt").read(),
+    url = "http://github.com/stephenmcd/django-forms-builder",
+    zip_safe = False,
+    include_package_data = True,
+    packages = find_packages(),
+    classifiers = [
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+    ]
+)
