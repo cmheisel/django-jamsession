1bc741cfaeedd8e2671ca198a367933ed089fa40
Chris Heisel <chris@heisel.org>
2010-12-23 18:22:18 -0500

    Killed abortive attempt to make mongoengine work with contrib.admin

--- jamsession/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from jamsession.models import DataSetDefinition
-
-
-class DataSetDefinitionAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register([DataSetDefinition], DataSetDefinitionAdmin)
