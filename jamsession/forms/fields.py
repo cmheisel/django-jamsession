5a74d1bdc7f32780b71d91a45094b254cc63a9f0
Chris Heisel <chris@heisel.org>
2011-03-12 17:19:42 -0500

    Raising an error if duplicate column names are provided.
    Explicit is better than implicit

--- jamsession/forms/fields.py
+++ jamsession/forms/fields.py
@@ -6,6 +6,15 @@ class SchemaField(forms.CharField):
     Custom field to accept a comma and line delimited
     text input of a schema definition
     """
+
+    def validate_key_value_pairs(self, key, value, schema):
+        if not(key and value):
+            msg = "Must provide both a column name and a column type"
+            raise forms.ValidationError(msg)
+        if key in schema:
+            msg = "Schema keys must be unique, more than one %s defined" % key
+            raise forms.ValidationError(msg)
+
     def parse_schema_entries(self, text):
         lines = [line for line in text.split('\n')]
         schema = {}
@@ -14,12 +23,8 @@ class SchemaField(forms.CharField):
             parts = line.split(',')
             if len(parts) >= 2:
                 key, value = parts[0].strip(), parts[1].strip()
-                if key and value:
-                    if key in schema:
-                        msg = "Schema keys must be unique, more than one %s defined" % key
-                        raise forms.ValidationError(msg)
-                    schema[key] = value
-
+                self.validate_key_value_pairs(key, value, schema)
+                schema[key] = value
         return schema
 
     def to_python(self, value):
