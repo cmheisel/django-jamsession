19e66f69024f529a9cbb23a9a891069dabd5771a
Chris Heisel <chris@heisel.org>
2011-03-13 09:33:19 -0400

    Separated TestCases for unit vs. functional

--- jamsession/test.py
+++ jamsession/test.py
@@ -2,8 +2,7 @@ from unittest2 import TestCase
 
 from django.test import TestCase as DjangoTestCase
 
-
-class JamTestCase(DjangoTestCase, TestCase):
+class JamTestCaseBase(DjangoTestCase, TestCase):
     """
     Base TestCase using encompassing functionality from
     both Django and unittest2
@@ -19,8 +18,13 @@ class JamTestCase(DjangoTestCase, TestCase):
         [db.drop_collection(name) for name in db.collection_names() \
           if 'system.' not in name]
 
+
+class JamTestCase(JamTestCaseBase):
     def _get_target_class(self):
         raise NotImplementedError
 
     def _make_one(self, *args, **kwargs):
         return self._get_target_class()(*args, **kwargs)
+
+class JamFuncTestCase(JamTestCaseBase):
+    pass
