c6cb162684ab9ff75022496aaf71ef1d86e451aa
Chris Heisel <chris@heisel.org>
2011-03-26 10:28:40 -0400

    Made setup.py less copy-and-pasty

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-jamsession",
-    version = __import__("jamsession").__version__,
-    author = "Chris Heisel",
-    author_email = "chris@heisel.org",
-    description = ("A Django reusable app providing the ability for admin "
-        "users to create their own forms."),
-    long_description = open("README.md").read(),
-    url = "http://github.com/stephenmcd/django-forms-builder",
-    zip_safe = False,
-    include_package_data = True,
-    packages = find_packages(),
-    classifiers = [
+    name="django-jamsession",
+    version=__import__("jamsession").__version__,
+    author="Chris Heisel",
+    author_email="chris@heisel.org",
+    description=("A Django application that allows users to create simple datastructures, store data in them collected via forms, load it from delimited files, and display it in simple views."),
+    long_description=open("README.md").read(),
+    url="http://github.com/cmheisel/django-jamsession",
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
